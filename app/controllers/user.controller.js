1978920b89806edde01dc90aa6d01c001c2b7cf0
Crane Warrior <132014889+cranewarrior@users.noreply.github.com>
2023-07-14 14:17:29 +0900

    Update user.controller.js for super user

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,7 +1,6 @@
-const axios = require('axios');
-
 const db = require("../models");
 const User = db.user;
+const superUserEmail = require("./../config/basic.config").superUserEmail;
 
 exports.getAllUsers = async (req, res) => {
   const users = await User.find().populate('group', 'name');
@@ -28,6 +27,22 @@ exports.deleteUsers = async (req, res) => {
 exports.updateUsers = async (req, res) => {
   try {
     const value = req.body.field == "allowed" ? req.body.value == "true" : req.body.value;
+        const superUser = await User.findOne({ email: superUserEmail });
+
+    if (req.body.selectedUsers.includes(superUser._id.toString())) {
+      return res.status(401).send({ message: `You can't modify super user ${superUserEmail}.` });
+    }
+
+    if (req.body.selectedUsers.includes(req.userId)) {
+      return res.status(401).send({ message: `You can't modify your account.` });
+    }
+
+    if (req.body.field === "role" && req.body.value === "user") {
+      const admins = await User.find({ role: "admin" });
+      if (admins.length === 1) {
+        return res.status(401).send({ message: `This is unique admin. You can't set it as user` });
+      }
+    }
     const result = await User.updateMany({ _id: { $in: req.body.selectedUsers } },
       { $set: { [req.body.field]: value } });
     res.status(200).send({ message: `${result.modifiedCount} users "${req.body.field}" set as ${req.body.value}.` });
